983920808a62082ef3e8816c754df86653a1b80d
unknown <sbaird02@CSPC009.dcs.bbk.ac.uk>
2016-04-22 15:27:22 +0100

    Start documentation

--- cw3-ray-tracer/build.sbt
+++ cw3-ray-tracer/build.sbt
@@ -8,6 +8,9 @@ lazy val akkaVersion = "2.4.2"
 
 resolvers += "Artima Maven Repository" at "http://repo.artima.com/releases"
 
+/*scalacOptions in (Compile, doc) ++= Seq("-doc-root-content", baseDirectory.value+"/root-doc.txt")
+*/
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
--- cw3-ray-tracer/src/main/scala/application/TracerConfiguration.scala
+++ cw3-ray-tracer/src/main/scala/application/TracerConfiguration.scala
@@ -1,24 +1,53 @@
-
-
 package application
 
 import com.mildlyskilled.Scene
-
+/**
+  * A data class to encapsulate the configuration parameters in the RayTracer system
+  *
+  */
 case class TracerConfiguration ()   {
   
-  
+/**
+  * The name of the input file that contains the scene definition.
+  *
+  */
   var infile : String = _
-  
+/**
+  * The name of the output file containing the resolved image.
+  *
+  */
   var outfile : String = _
   
+/**
+  * A Scene object representing the parsed contents of the input file. For 
+  * efficiency this should be used to clone new Scene instances.
+  *
+  */
   var scene: Scene = _
   
+/**
+  * The number of worker actors to which the task is delegated. 
+  *
+  */
   var workers : Int = _
   
+/**
+  * The number of units that the task will be broken into. 
+  *
+  */
   var workUnits : Int = _ 
   
+/**
+  * The dimensions of the image to build. The values represent the x-dimension
+  * and the y-dimension respectively.
+  *
+  */  
   var dimensions : (Int,Int) = _
   
+/**
+  * The anti-aliasing factor to use in the ray tracing algorithm
+  *
+  */    
   var antiAliasingFactor : Int = _
   
   
